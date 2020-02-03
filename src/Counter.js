85a13d18af0de787f3aecc31317b9853b62d7e42
Aaron Marr <aaron.marr@icloud.com>
2020-02-03 21:56:01 +0000

    Refactor Counter component into class-based component

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { CounterConsumer } from './CounterContext';
 
-const Counter = () => (
-  // Here we're implementing the consumer, which will give us access
-  // to anything which was defined in the provider context.
-  // The consumer accepts a single function as it's child. This function
-  // should be used to return any elements or fragments.
-  // The first argument of this function, is whatever we passed into the
-  // value property of the provider. In this case, it's an object with
-  // 3 properties which we can destructure and utilise within this component
-  <CounterConsumer>
-    {({ counter, onIncrement, onDecrement }) => (
-      <div className="counter">
-        <h1 className="counter__count">Count: {counter}</h1>
-        <button className="counter__inc" onClick={onIncrement}>+</button>  
-        <button className="counter__dec" onClick={onDecrement}>-</button>   
-      </div>
-    )}
-  </CounterConsumer>
-);
+class Counter extends React.Component {
+  render() {
+    return (
+      // Here we’re implementing the consumer, which will give 
+      // us access to values defined in the provider context.
+      // The consumer accepts a single function as it’s child,  
+      // and should return any elements or fragments.
+      // The first argument of this function is whatever is passed 
+      // into the 'value' property of the provider. In this case, 
+      // it’s an object with 3 properties. We can destructure 
+      // these using the render props pattern.
+      <CounterConsumer>
+        {({ counter, onIncrement, onDecrement }) => (
+          <div className="counter">
+            <h1 className="counter__count">Count: {counter}</h1>
+            <button className="counter__inc" onClick={onIncrement}>+</button>  
+            <button className="counter__dec" onClick={onDecrement}>-</button>   
+          </div>
+        )}
+      </CounterConsumer>
+    )
+  }
+};
 
 export default Counter;
